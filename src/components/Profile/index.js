fe32d723e078ded69beb50d3d06551e72e69b146
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-30 00:37:31 +0300

    style

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -27,6 +27,10 @@ const Profile = () => {
   };
 
 
+ 
+  useEffect(() => {
+    getDataLS();
+  }, []);
   useEffect(() => {
     getData();
     // eslint-disable-next-line
@@ -63,32 +67,7 @@ const Profile = () => {
       {account.map((item, i) => {
         return (
           <div>
-            <div className="navbar-top">
-              <div className="title">
-                <h1>Profile</h1>
-              </div>
-             
-              <ul>
-                <li>
-                  <a href="#message">
-                    <span className="icon-count">29</span>
-                    <i className="fa fa-envelope fa-2x" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#notification">
-                    <span className="icon-count">59</span>
-                    <i className="fa fa-bell fa-2x" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#sign-out">
-                    <i className="fa fa-sign-out-alt fa-2x" />
-                  </a>
-                </li>
-              </ul>
-            
-            </div>
+           
           
             <div className="sidenav">
               <div className="profile">
